6a4056586d08a2dc42c66909df8ddc1670ee53d4
John Suico <johncdsuico@gmail.com>
2020-07-24 10:55:02 -0700

    create navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Router>
+        <nav className="navbar">
+          {/* <div className="container"> */}
+            <div className="navbar-items">
+              <div className="nav-logo">
+                <Link to="/" className="nav-link">Haven</Link>
+              </div>
+              <div className="nav-links">
+                <Link to="/posts" className="nav-link">Posts</Link>
+                <Link to="/create" className="nav-link">Write</Link>
+              </div>
+            </div>
+          {/* </div> */}
+        </nav>
+
+        <Switch>
+          <Route path="/" exact>
+            {/* Home Page */}
+          </Route>
+          <Route path="/posts">
+            {/* Posts Page */}
+          </Route>
+          <Route path="/create">
+            {/* Create post page */}
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
