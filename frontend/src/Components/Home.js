26d55c0f5c36f98ccd904784641561c15a474b12
John Suico <johncdsuico@gmail.com>
2020-07-24 11:18:32 -0700

    finish home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -12,11 +12,11 @@ function Home() {
           <div className="hero-caption">
             <h1>Haven</h1>
           </div>
-          <div className="sub-caption">
-            <h3>speak your mind anonymously.</h3>
+          <div className="hero-subcaption">
+            <h2>speak your mind anonymously.</h2>
           </div>
           <div className="hero-cta">
-            <Link to="/create">start writing</Link>
+            <Link to="/create" className="hero-cta-btn">start writing</Link>
           </div>
         </div>
       </div>
