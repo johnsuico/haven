081af792be835ca3e024692972480641290f2fc2
John Suico <johncdsuico@gmail.com>
2020-07-25 17:40:01 -0700

    finish create form mockup

--- frontend/src/Components/Create-component/Create.js
+++ frontend/src/Components/Create-component/Create.js
@@ -8,7 +8,30 @@ function Create() {
     <div className="bg">
       <div className="create">
         <div className="create-container">
-          <h1 className="create-title">Create your anonymouse post.</h1>
+          <h1 className="create-title">Create your anonymous post</h1>
+            <form className="create-form">
+              <div className="create-form-section">
+                <label className="create-form-label">
+                  Title
+                </label>
+                <input type="text" className="create-form-text-input"/>
+              </div>
+              <div className="create-form-section">
+                <label className="create-form-label">
+                  Purpose
+                </label>
+                <input type="text" className="create-form-text-input"/>
+              </div>
+              <div className="create-form-section">
+                <label className="create-form-label">
+                  Body
+                </label>
+                <textarea className="create-form-textarea" rows="22"></textarea>
+              </div>
+              <div className="form-submit">
+                <input type="submit" value="Post" className="create-form-submit"/>
+              </div>
+            </form>
         </div>
       </div>
     </div>
