f6c0cb23b3f2eabe5b0a5b8a4c1d0ef742ff2cc2
John Suico <johncdsuico@gmail.com>
2020-07-26 11:27:43 -0700

    add axios post route to backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 require('dotenv').config();
@@ -8,6 +9,7 @@ require('dotenv').config();
 const postModel = require('./models/posts.model');
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.on('connected', () => {
--- frontend/src/Components/Create-component/Create.js
+++ frontend/src/Components/Create-component/Create.js
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 // CSS
 import './create.css';
 
 function Create() {
+
+  const [title, setTitle] = useState('');
+  const [purpose, setPurpose] = useState('');
+  const [body, setBody] = useState('');
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+  function handlePurposeChange(event) {
+    setPurpose(event.target.value);
+  }
+  function handleBodyChange(event) {
+    setBody(event.target.value);
+  }
+
+  function handleCreateSubmit(event) {
+    Axios.post('http://localhost:5000/api/haven', {
+      title: title,
+      purpose: purpose,
+      body: body
+    })
+    .then((res) => {
+      console.log("Successfully sent axios request");
+    })
+
+    event.preventDefault();
+  }
+
   return(
     <div className="bg">
       <div className="create">
         <div className="create-container">
           <h1 className="create-title">Create your anonymous post</h1>
-            <form className="create-form">
+            <form className="create-form" onSubmit={handleCreateSubmit}>
               <div className="create-form-section">
                 <label className="create-form-label">
                   Title
                 </label>
-                <input type="text" className="create-form-text-input"/>
+                <input type="text" className="create-form-text-input" value={title} onChange={handleTitleChange} required/>
               </div>
               <div className="create-form-section">
                 <label className="create-form-label">
                   Purpose
                 </label>
-                <input type="text" className="create-form-text-input"/>
+                <input type="text" className="create-form-text-input" onChange={handlePurposeChange} value={purpose}/>
               </div>
               <div className="create-form-section">
                 <label className="create-form-label">
                   Body
                 </label>
-                <textarea className="create-form-textarea" rows="22"></textarea>
+                <textarea className="create-form-textarea" rows="22" value ={body} onChange={handleBodyChange} required></textarea>
               </div>
               <div className="form-submit">
                 <input type="submit" value="Post" className="create-form-submit"/>
