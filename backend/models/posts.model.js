60e2b03aec909a38e9ea689c3c3b7504ca15b11e
John Suico <johncdsuico@gmail.com>
2020-07-27 13:11:05 -0700

    add upvote and downvote fields

--- backend/models/posts.model.js
+++ backend/models/posts.model.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema ({
   title: String,
   purpose: String,
-  body: String
+  body: String,
+  upvote: {
+    type: Number,
+    default: 0
+  },
+  downvote: {
+    type: Number,
+    default: 0
+  }
 })
 
 const Post = mongoose.model('Post', postSchema);
