f4b25f9860a5247b22cd41c1c36f08d2762dae70
John Suico <johncdsuico@gmail.com>
2020-07-27 09:36:34 -0700

    create connection to backend to retrieve posts

--- frontend/src/Components/PostList-component/PostList.js
+++ frontend/src/Components/PostList-component/PostList.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 // CSS
 import './postList.css';
 
 function PostList() {
+
+  const [havenArray, setHavenArray] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:5000/api/haven')
+      .then( response => {
+        setHavenArray(response.data);
+      })
+  })
+
   return (
-    <div>
-      <h1>Post List Component</h1>
+    <div className="bg">
+      <div className="postList">
+        <div className="postList-container">
+          <h2 className="postList-title">Anonymous Posts</h2>
+          <div className="postList-posts">
+            {havenArray.map(haven => 
+              <div className="havenContainer" key={haven._id}>
+                <h3>{haven.title}</h3>
+                <h4>{haven.purpose}</h4>
+                <p className="havenBody">{haven.body}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
