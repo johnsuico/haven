76ecc3836de1e4ca7ea16a46ab793d9aad7c4e90
John Suico <johncdsuico@gmail.com>
2020-07-30 17:51:15 -0700

    create specificPost component

---
+++ frontend/src/Components/SpecificPost-component/SpecificPost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SpecificPost() {
+  return(
+    <div className="bg">
+      <div className="specifcPost-container">
+        <h2>Specific Post Page</h2>
+      </div>
+    </div>
+  )
+}
+
+export default SpecificPost;
