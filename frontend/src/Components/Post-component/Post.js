db527607b80382aeac1a33f10b0859224db3df67
John Suico <johncdsuico@gmail.com>
2020-07-30 18:22:56 -0700

    update specificpost component

--- frontend/src/Components/Post-component/Post.js
+++ frontend/src/Components/Post-component/Post.js
@@ -15,8 +15,10 @@ function Post(props) {
     Axios.put(`http://localhost:5000/api/haven/downvote/${props.id}`);
   }
 
+  const link = `/haven/${props.id}`;
+
   return (
-    <Link to="/" className="haven-link">
+    <Link to={link} className="haven-link">
       <div className="havenContainer">
         <h3 className="haven-title haven-text">{props.title}</h3>
         <h4 className="haven-purpose haven-text">{props.purpose}</h4>
--- frontend/src/Components/SpecificPost-component/SpecificPost.js
+++ frontend/src/Components/SpecificPost-component/SpecificPost.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { useParams } from 'react-router-dom'
+
+// CSS
+import './specificPost.css';
 
 function SpecificPost() {
+
+  const { id } = useParams(); 
+
+  const [haven, setHaven] = useState();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect( ()=> {
+    Axios.get(`http://localhost:5000/api/haven/${id}`)
+      .then(response => {
+        setHaven(response.data);
+        setLoading(false);
+      })
+  })
+
   return(
     <div className="bg">
-      <div className="specifcPost-container">
-        <h2>Specific Post Page</h2>
+      <div className="specificPost">
+        <div className="specificPost-container">
+
+        </div>
       </div>
     </div>
   )
