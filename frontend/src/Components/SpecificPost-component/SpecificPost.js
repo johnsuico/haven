1fa4b15dff393b9aa9ddeb89926a2387902ed91e
John Suico <johncdsuico@gmail.com>
2020-07-31 22:10:25 -0700

    add downvote and upvote to specificPost pages

--- frontend/src/Components/SpecificPost-component/SpecificPost.js
+++ frontend/src/Components/SpecificPost-component/SpecificPost.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import Axios from 'axios';
 import { useParams } from 'react-router-dom'
 
@@ -17,12 +18,40 @@ function SpecificPost() {
       })
   })
 
+  function upvoteClick() {
+    Axios.put(`http://localhost:5000/api/haven/upvote/${haven._id}`);
+  }
+  function downvoteClick() {
+    Axios.put(`http://localhost:5000/api/haven/downvote/${haven._id}`);
+  }
+
   return(
     <div className="bg">
       <div className="specificPost">
         <div className="specificPost-container">
           <h2 className="specificPost-title">{haven.title}</h2>
-          <h2 className="specificPost-purpose">Purpose: {haven.purpose}</h2>
+
+          {/* Second Level of headings */}
+          <div className="specificPost-second-level-container">
+
+            {/* Post Purpose */}
+            <h2 className="specificPost-purpose">Purpose: {haven.purpose}</h2>
+
+            {/* Icon container */}
+            <div className="specificPost-icon-container">
+              <div className="upVote-container" onClick={upvoteClick}>
+                <FaArrowUp />
+                <p className="upvote">{haven.upvote}</p>
+              </div>
+              <div className="downVote-container" onClick={downvoteClick}>
+                <FaArrowDown />
+                <p className="downvote">{haven.downvote}</p>
+              </div>
+            </div>
+
+          </div>
+
+          {/* Post Body Container */}
           <div className="specificPost-context-container">
             <h2>Body:</h2>
             <p>{haven.body}</p>
